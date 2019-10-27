3e0588e749350860b99ace75790434737198c6a4
Raymond Yang <raymond.yang@mail.mcgill.ca>
2019-10-26 21:28:15 -0400

    lab 3

---
+++ G64_Lab3/drivers/inc/LEDs.h
@@ -0,0 +1,5 @@
+#ifndef _LEDs
+#define	_LEDs
+	extern int read_LEDs_ASM();
+	extern int write_LEDs_ASM(int x);
+#endif
---
+++ G64_Lab3/drivers/inc/slider_switches.h
@@ -0,0 +1,4 @@
+#ifndef _SLIDER_SWITCHES
+#define _SLIDER_SWITCHES
+	extern int read_slider_switches_ASM();
+#endif
